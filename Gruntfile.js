c52fbebc345ec1454727dd3312df47511c5663bc
Ion Gireada <shytiger@yahoo.com>
2024-12-24 11:53:55 +0200

    * chore(Gruntfile.js): remove Gruntfile.js * chore(package.json): remove grunt and related dependencies

--- Gruntfile.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(grunt) {
-    grunt.loadNpmTasks('grunt-generate-cli-markdown');
-    grunt.loadNpmTasks('grunt-insert-file-tag');
-
-    grunt.registerTask('default', ['generate-cli-markdown', 'insert_file_tag']);
-};
